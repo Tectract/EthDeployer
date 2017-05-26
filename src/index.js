0a2a137af3e28b5c87c22ae0e192ebf49360374e
locutus <ryan@molecke.com>
2017-05-26 03:51:47 -0600

    updated to use BrowserSolc for compilation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Deploy from './Deploy';
 import './index.css';
 
+//console.debug(window);
+
 ReactDOM.render(
   <Deploy />,
   document.getElementById('root')
